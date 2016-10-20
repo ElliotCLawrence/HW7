db9943efd9786b08d9e8f5a5d3892cfc4ee9d9a0
elliotclawrence <elliotclawrence@gmail.com>
2016-10-19 21:46:34 -0700

    Updated file to actual hw 7

--- HW3 Test/WebServer.cs
+++ HW3 Test/WebServer.cs
@@ -1,7 +1,7 @@
 ﻿//Elliot Lawrence 
 //CS 422
-//HW 3
-//9_16_2016 
+//HW 7
+//10/15/2016
 
 using System;
 using System.Net;
@@ -16,7 +16,6 @@ namespace CS422
 {
     class WebServer
     {
-
         static ThreadPoolRouter threadPool;
         static DemoService demo;
         private static List<WebService> webServices = new List<WebService>();
@@ -58,7 +57,6 @@ namespace CS422
             {
                 Console.WriteLine("Listener ending");
             }
-            
         }
 
         public static void doThreadWork() //must cast to object in order to use parameterized thread start
@@ -68,7 +66,6 @@ namespace CS422
 
             while (true)
             {
-              
                 client = threadPool.takeClient();
 
                 if (client == null) //if null client break this thread
@@ -95,7 +92,6 @@ namespace CS422
 
                 if (!found)
                     request.WriteNotFoundResponse("404 Page Not Found");
-
             }
         }
 
@@ -103,18 +99,43 @@ namespace CS422
         private static WebRequest BuildRequest(TcpClient client)
         {
             NetworkStream clientStream = client.GetStream();
-
+            int ammountRead = 0;
+            clientStream.ReadTimeout = (int) new TimeSpan(0, 0, 2).TotalMilliseconds;
+            int startingSeconds = DateTime.Now.Second;
+            int startingMinute = DateTime.Now.Minute;
             string fullRequest = "";
             string destination = "/";
             byte[] streamBuff = new byte[1024]; //create a buffer for reading
             int x = clientStream.Read(streamBuff, 0, 1024);
             int y = 0;
             int i = 0; //index of streamBuff
-
+            ammountRead += x;
             string validReq = "GET / HTTP/1.1\r\n"; //I'm using this string to check against the request.
 
-            while (x > 0 && y < validReq.Length)
-            { //while the ammount read is greater than 0 bytes
+            while (x > 0 && y < validReq.Length) //while the ammount read is greater than 0 bytes
+            { 
+                if (ammountRead > 2048)
+                {
+                    clientStream.Close();
+                    client.Close();
+                    return null;
+                }
+                if (DateTime.Now.Minute != startingMinute)//if it's a different minute, add 60 seconds when you check change in time
+                {
+                    if (DateTime.Now.Second + 60 - startingSeconds > 10) //if 10 seconds has past since started reading.
+                    {
+                        clientStream.Close();
+                        client.Close();
+                        return null;
+                    }
+                }
+                else if (DateTime.Now.Second - startingSeconds > 10) //if 10 seconds has past since started reading.
+                {
+                    clientStream.Close();
+                    client.Close();
+                    return null;
+                }
+
                 i = 0;
                 fullRequest += Encoding.Default.GetString(streamBuff);
                 while (i < x && y < validReq.Length)
@@ -140,18 +161,25 @@ namespace CS422
                 }
 
                 if (y < validReq.Length) //only read if you need to.
+                {
                     x = clientStream.Read(streamBuff, 0, 1024);//read next bytes
+                    ammountRead += x;
+                }
+
                 else //otherwise, break the loop
                     break;
             }
 
-
-
-
             //read in the headers
             while (true)
             {
-               
+                if (ammountRead > 102400)
+                {
+                    clientStream.Close();
+                    client.Close();
+                    return null;
+                }
+
                 if (fullRequest.Contains("\r\n\r\n"))
                 {
                     break;
@@ -159,14 +187,30 @@ namespace CS422
                 
                 else
                 {
+                    //read more stuff
                     x = clientStream.Read(streamBuff, 0, 1024); //read in more
-                    fullRequest += System.Text.Encoding.Default.GetString(streamBuff);
+                    ammountRead += x;
+                    fullRequest += Encoding.Default.GetString(streamBuff);
+
+                    if (DateTime.Now.Minute != startingMinute) //if it's a different minute, add 60 seconds when you check change in time
+                    {
+                        if (DateTime.Now.Second + 60 - startingSeconds > 10) //if 10 seconds has past since started reading.
+                        {
+                            clientStream.Close();
+                            client.Close();
+                            return null;
+                        }
+                    }
+                    else if (DateTime.Now.Second - startingSeconds > 10) //if 10 seconds has past since started reading.
+                    {
+                        clientStream.Close();
+                        client.Close();
+                        return null;
+                    }
                 }
 
                 if (x <= 0)
-                    return null; //never had headers. invalid request!
-
-
+                    return null; //never had headers, nor a second \r\n. invalid request!
             }
 
             string onlyHeaders;
@@ -182,7 +226,6 @@ namespace CS422
             }
 
             List<Tuple<string, string>> headerList = new List<Tuple<string, string>>();
-
 
             string headers = onlyHeaders.Substring(0, endHeadersCount);
             string[] splitters = new string[1];
@@ -222,7 +265,6 @@ namespace CS422
             return request;
         }
 
-
         public static void AddService(WebService service)
         {
             webServices.Add(service);
@@ -238,8 +280,6 @@ namespace CS422
 
             return;
         }
-
-
     }
 }
 
